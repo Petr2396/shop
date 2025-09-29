0913170f3042f54e10d2f721d558e016486b8f44
Petr2396 <115742461+Petr2396@users.noreply.github.com>
2025-09-29 14:06:24 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path("profile/edit/", views.edit_profile, name="edit_profile"),
     
     # стандартные логин/логаут
-    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+     path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='registration/login.html'),
+        name='login'
+    ),
     path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 
     # регистрация
